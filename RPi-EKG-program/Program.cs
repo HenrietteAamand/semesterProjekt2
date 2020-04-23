b1bb866eceb32e762cdcede488a1b26a5ad8b7aa
LarsMoelgaard <60734425+LarsMoelgaard@users.noreply.github.com>
2020-04-23 10:57:14 +0200

    Oprettelse af RPi-projekt

---
+++ RPi-EKG-program/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using RaspberryPiCore.ADC;
+using RaspberryPiCore.TWIST;
+using RaspberryPiCore.LCD;
+
+
+namespace Raspberry_Pi_Dot_Net_Core_Console_Application3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
