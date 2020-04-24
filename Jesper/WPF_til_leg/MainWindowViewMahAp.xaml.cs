d352973c7f697b339b7e7b8bd76958beffac34cd
Jesper Trøiborg <jespertroiborg@hotmail.com>
2020-04-24 14:59:43 +0200

    Eksperimenteret med MehApp. Designet en del MainWindowViewModel. Tilføjet mehApp template: Tilføjet WindowsCommands - Settings og Updates+Bagde. Tilføjet ListBoxes til Patienter og tilhørende ECG's: Tilføjet søgefelt. Oprettet dummy-objekter i main + PatientModel til test

---
+++ Jesper/WPF_til_leg/MainWindowViewMahAp.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MahApps.Metro.Controls;
+using MahApps.Metro.Controls.Dialogs;
+
+namespace WPF_til_leg
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public int updates = 10;
+        public List<PatientModel> Patients { get; set; }
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            showDialog();
+            updateBadge.Badge = updates;
+            PatientModel patient1 = new PatientModel {Name = "jens", PatientCPR = "123456"  };
+            Patients = new List<PatientModel>();
+            Patients.Add(patient1);
+            this.DataContext = Patients;
+      
+            
+
+        }
+
+        async Task showDialog()
+        {
+            var result = await this.ShowMessageAsync("Welcome", $"There are {updates} new ECG's. Do you wanna update?", MessageDialogStyle.AffirmativeAndNegative);
+
+            if (result == MessageDialogResult.Affirmative)
+            {
+                await this.ShowMessageAsync($" {updates} ECG's have been updated","");
+            }
+
+        }
+    }
+}
---
+++ Jesper/WPF_til_leg/PatientModel.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_til_leg
+{
+    public class PatientModel
+    {
+
+            private string _patientCPR;
+            private string _name;
+
+            public string PatientCPR
+        {
+            get => this._patientCPR;
+            set
+            {
+                this._patientCPR = value;
+            }
+        }
+            public string Name
+            {
+                get => this._name;
+            set
+            {
+                this._name = value;
+            }
+        }
+
+    }
+}
