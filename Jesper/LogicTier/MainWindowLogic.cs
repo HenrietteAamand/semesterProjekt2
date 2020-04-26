42cd7fa69ead94717bdd432cf569fad120f19999
amaliekjaersgaard <amaliekjaersgaard@AMALIEKJRSG01AE>
2020-04-26 12:53:38 +0200

    datalag-klasser gjort public

--- Jesper/LogicTier/MainWindowLogic.cs
+++ Jesper/LogicTier/MainWindowLogic.cs
@@ -6,14 +6,28 @@ namespace LogicTier
 {
     public class MainWindowLogic
     {
+		private AnalyzeECG analyzeECG;
 		public PatientModel patientRef { get; private set; }
 		public AnalyzedECGModel aECGRef { get; private set; }
 		public List<AnalyzedECGModel> aECGList { get; private set; }
+		public List<PatientModel> patientList { get; private set; }
 
 		public MainWindowLogic()
 		{
 
 		}
+
+		public void LoadPatient(String cpr) {}
+
+		public void UpdatePatientList() {}
+
+		public void UpdateAECGList() {}
+
+		public void UploadData() { }
+
+		public void SelectECG() { }
+
+		public void SelectPatient() { }
 
 	}
 }
--- Jesper/LogicTier/SetupWindowLogic.cs
+++ Jesper/LogicTier/SetupWindowLogic.cs
@@ -6,5 +6,6 @@ namespace LogicTier
 {
     class SetupWindowLogic
     {
+
     }
 }
