8e0db8a0d7ea374b9664f7c2ccff7c151e6dc452
amaliekjaersgaard <amaliekjaersgaard@AMALIEKJRSG01AE>
2020-04-26 12:42:45 +0200

    Public analyzedECGModel

--- Jesper/DataTier/Models/AnalyzedECGModel.cs
+++ Jesper/DataTier/Models/AnalyzedECGModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace DataTier.Models
 {
-    class AnalyzedECGModel
+    public class AnalyzedECGModel
     {
     }
 }
--- Jesper/LogicTier/MainWindowLogic.cs
+++ Jesper/LogicTier/MainWindowLogic.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using DataTier.Models;
+using System;
+using System.Collections.Generic;
 
 namespace LogicTier
 {
     public class MainWindowLogic
     {
+		public PatientModel patientRef { get; private set; }
+		public AnalyzedECGModel aECGRef { get; private set; }
+		public List<AnalyzedECGModel> aECGList { get; private set; }
+
+		public MainWindowLogic()
+		{
+
+		}
 
 	}
 }
