9785ec0352d36053a7a7df9a2f332613bf36c814
Jesper Trøiborg <jespertroiborg@hotmail.com>
2020-04-26 14:04:32 +0200

    Added methods, attributes, ctor and props to ECGModel and PatientModel.

--- Jesper/DataTier/Models/ECGModel.cs
+++ Jesper/DataTier/Models/ECGModel.cs
@@ -4,7 +4,7 @@ using System.Text;
 
 namespace DataTier.Models
 {
-    class ECGModel
+    public class ECGModel
     {
         #region Attributes
 
--- Jesper/DataTier/Models/PatientModel.cs
+++ Jesper/DataTier/Models/PatientModel.cs
@@ -12,36 +12,87 @@ namespace DataTier.Models
         #endregion
 
         #region Properties
+        private int id;
+
+        public int ID
+        {
+            get { return id; }
+            private set { id = value; }
+        }
+
+        private int ecgMonitorID;
+
+        public int ECGMonitorID
+        {
+            get { return ecgMonitorID; }
+            set { ecgMonitorID = value; }
+        }
+
+        private String cpr;
+
+        public String CPR
+        {
+            get { return cpr; }
+            set { cpr = value; }
+        }
+
+        private String firstName;
+
+        public String FirstName
+        {
+            get { return firstName; }
+            set { firstName = value; }
+        }
+
+        private String lastName;
+
+        public String LastName
+        {
+            get { return lastName; }
+            set { lastName = value; }
+        }
+
+        private List<ECGModel> ecgList;
+
+        public List<ECGModel> ECGList
+        {
+            get { return ecgList; }
+            set { ecgList = value; }
+        }
+
+        private List<AnalyzedECGModel> aECGList;
+
+        public List<AnalyzedECGModel> AECGList
+        {
+            get { return aECGList; }
+            set { aECGList = value; }
+        }
+
 
         #endregion
 
         #region Constructor
+        public PatientModel()
+        {
+
+        }
+
+        public PatientModel(int id, int ecgMonitorID, string cpr, string firstName, string lastName,
+            List<ECGModel> ecgList, List<AnalyzedECGModel> aECGList)
+        {
+            ID = id;
+            ECGMonitorID = ecgMonitorID;
+            CPR = cpr;
+            LastName = lastName;
+            ECGList = ecgList;
+            AECGList = aECGList;
+        }
 
         #endregion
 
         #region Methods
 
         #endregion
-
-        private string _patientCPR;
-        private string _name;
-
-        public string PatientCPR
-        {
-            get => this._patientCPR;
-            set
-            {
-                this._patientCPR = value;
-            }
-        }
-        public string Name
-        {
-            get => this._name;
-            set
-            {
-                this._name = value;
-            }
-        }
 
     }
 }
