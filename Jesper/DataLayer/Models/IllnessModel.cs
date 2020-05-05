a16c42f8003fdc8794a38a2e11e7795cb589fd50
Jesper Trøiborg <jespertroiborg@hotmail.com>
2020-05-05 16:03:11 +0200

    Removed intervalRtak

--- Jesper/DataLayer/Models/IllnessModel.cs
+++ Jesper/DataLayer/Models/IllnessModel.cs
@@ -8,7 +8,6 @@ namespace DataTier.Models
         private int id;
         private string name;
         private string about;
-        private TimeSpan intervalR;
         private bool stSegmentElevated;
         private bool stSegmentDepressed;
 
@@ -19,12 +18,11 @@ namespace DataTier.Models
         #endregion
 
         #region Constructor
-        public IllnessModel(int id, string name, string about, TimeSpan intervalR, bool stSegmentElevated, bool stSegmentDepressed)
+        public IllnessModel(int id, string name, string about, bool stSegmentElevated, bool stSegmentDepressed)
         {
             this.id = id;
             this.name = name;
             this.about = about;
-            this.intervalR = intervalR;
             this.stSegmentElevated = stSegmentElevated;
             this.stSegmentDepressed = stSegmentDepressed;
 
