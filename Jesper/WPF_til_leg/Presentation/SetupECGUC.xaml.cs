4b577e4303fe3b0be7bb5cc333f228ba99070e20
Sarah Amalie Vestmark Pind <au554980@uni.au.dk>
2020-05-05 16:40:50 +0200

    Changes in SetupECGUC.xaml.cs

--- Jesper/WPF_til_leg/Presentation/SetupECGUC.xaml.cs
+++ Jesper/WPF_til_leg/Presentation/SetupECGUC.xaml.cs
@@ -1,19 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-//using System.Windows.Forms;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using LogicTier;
+using Models.Models;
+
+//using System.Windows.Forms;
 
 namespace WPF_til_leg.Presentation
 {
@@ -22,12 +12,16 @@ namespace WPF_til_leg.Presentation
     /// </summary>
     public partial class SetupECGUC : UserControl
     {
+        private SetupWindowLogic setupObj;
+        
+        
+
         public SetupECGUC()
         {
             InitializeComponent();
-            
 
-    }
+            setupObj = new SetupWindowLogic();
+        }
 
         //UC2 + UC4
         private void LinkECGB_Click(object sender, RoutedEventArgs e)
@@ -42,16 +36,18 @@ namespace WPF_til_leg.Presentation
             System.Windows.MessageBox.Show("Nulstilling gennemført.");
         }
 
+        // Tilknyt patient eller nulstil EKG-måler:
         private void EcgCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            //if (EcgCB.SelectedItem())
+            //ECGMonitorModel ecgMoniter = new ECGMonitorModel(EcgCB.Text);
+            //if (setupObj.monitorInUse(EcgCB) == false)
             //{
             //    ResetECGB.IsEnabled = false;
             //    System.Windows.MessageBox.Show("EKG-måler er ikke i brug");
             //}
             //else
             //{
-            //    LinkECGB.IsEnabled = false;
+            //    LinkECGB.IsEnabled = true;
             //    System.Windows.MessageBox.Show("EKG-måler er i brug");
             //}
         }
