74856c927532379c8b262275ce5118f54267b830
Sarah Amalie Vestmark Pind <au554980@uni.au.dk>
2020-05-05 13:38:51 +0200

    Minor bug fixes

--- Jesper/WPF_til_leg/Presentation/SetupECGUC.xaml.cs
+++ Jesper/WPF_til_leg/Presentation/SetupECGUC.xaml.cs
@@ -7,11 +7,13 @@ using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+//using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using LogicTier;
 
 namespace WPF_til_leg.Presentation
 {
@@ -23,7 +25,35 @@ namespace WPF_til_leg.Presentation
         public SetupECGUC()
         {
             InitializeComponent();
+            
+
+    }
+
+        //UC2 + UC4
+        private void LinkECGB_Click(object sender, RoutedEventArgs e)
+        {
+            //LinkECGToPatient();
+            System.Windows.MessageBox.Show("Tilknytning gennemført.");
         }
 
+        private void ResetECGB_Click(object sender, RoutedEventArgs e)
+        {
+            //LinkECGToPatient();
+            System.Windows.MessageBox.Show("Nulstilling gennemført.");
+        }
+
+        private void EcgCB_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //if (EcgCB.SelectedItem())
+            //{
+            //    ResetECGB.IsEnabled = false;
+            //    System.Windows.MessageBox.Show("EKG-måler er ikke i brug");
+            //}
+            //else
+            //{
+            //    LinkECGB.IsEnabled = false;
+            //    System.Windows.MessageBox.Show("EKG-måler er i brug");
+            //}
+        }
     }
 }
