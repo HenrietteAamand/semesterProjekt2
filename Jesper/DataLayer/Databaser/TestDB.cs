d71eac82e91299553fc90b6cd9434c5e1aac0a04
Jesper Trøiborg <jespertroiborg@hotmail.com>
2020-05-08 11:17:45 +0200

    Stashed

---
+++ Jesper/DataLayer/Databaser/TestDB.cs
@@ -0,0 +1,97 @@
+﻿using DataTier.Interfaces;
+using Models.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTier.Databaser
+{
+    public class TestDB : ILocalDatabase
+    {
+        ECGModel ecg1 = new ECGModel("123456-7890", 1,( new DateTime(2011,1,1)), 50,
+            (new List<double> { 4, 4, 4, 5, 4, 3.5, 9, 1, 2, 4, 4, 4.5, 4, 4 }), 1, false);
+        
+        AnalyzedECGModel aECG = new AnalyzedECGModel("123456-7890", 1, 1,(new DateTime(2011, 1,1)), 50,
+            (new List<double> { 4, 4, 4, 5, 4, 3.5, 9, 1, 2, 4, 4, 4.5, 4, 4 }), 1, false, 
+            (new IllnessModel(1, "st", "not good", 2, 4, false, false)),
+            new List<double> { 4, 4, 4, 5, 4, 3.5, 4.5, 4, 4 });
+        ECGMonitorModel monitor = new ECGMonitorModel(1, false);
+        PatientModel patient = new PatientModel("123456-7890", "Peter", "Petersen");
+        IllnessModel illness = new IllnessModel(1, "st", "not good", 2, 4, false, false);
+        List<PatientModel> patientList = new List<PatientModel>();
+        
+        List<AnalyzedECGModel> aECGList = new List<AnalyzedECGModel>();
+        List<ECGModel> ecgList = new List<ECGModel>();
+        List<ECGMonitorModel> monitorList = new List<ECGMonitorModel>();
+        List<IllnessModel> illnessList = new List<IllnessModel>();
+        public TestDB()
+        {
+            aECG.STStartIndex = 1;
+            aECGList.Add(aECG);
+            ecgList.Add(ecg1);
+            patientList.Add(patient);
+            monitorList.Add(monitor);
+            illnessList.Add(illness);
+        }
+
+        public void CreatePatient(PatientModel patient)
+        {
+            patientList.Add(patient);
+        }
+
+        public List<AnalyzedECGModel> GetAllAnalyzedECGs()
+        {
+            return aECGList;
+        }
+
+        public List<ECGMonitorModel> GetAllECGMonitors()
+        {
+            return monitorList;
+        }
+
+        public List<ECGModel> GetAllECGs()
+        {
+            return ecgList;
+        }
+
+        public List<IllnessModel> GetAllIllnesses()
+        {
+            return illnessList;
+        }
+
+        public List<PatientModel> GetAllPatients()
+        {
+            return patientList;
+        }
+
+        public void UpdateIsAnalyzed(int ecgID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateIsRead(int aECGID)
+        {
+            foreach (var item in collection)
+            {
+
+            }
+        }
+
+        public void UpdateLinkECGToPatient(string cpr, int ecgMonitorID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UpdateResetECGMonitor(int ecgMonitorID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void UploadAnalyzedECGs(AnalyzedECGModel analyzedEcg)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
