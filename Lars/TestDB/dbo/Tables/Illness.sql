78f5c812f9b0f138e6c0b5a39ef49ee0b784423d
Jesper Trøiborg <jespertroiborg@hotmail.com>
2020-05-09 15:13:08 +0200

    Cloned DB from sky.

---
+++ Lars/TestDB/dbo/Tables/Illness.sql
@@ -0,0 +1,11 @@
+﻿CREATE TABLE [dbo].[Illness] (
+    [ID]                 INT            NOT NULL,
+    [About]              NVARCHAR (MAX) NULL,
+    [stMax]              FLOAT (53)     NULL,
+    [srMax]              FLOAT (53)     NULL,
+    [STSegmentElevated]  BIT            NULL,
+    [STSegmentDepressed] BIT            NULL,
+    [Name]               NVARCHAR (20)  NOT NULL,
+    PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
---
+++ Lars/TestDB/dbo/Tables/Patient.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Patient] (
+    [CPR]       NCHAR (11)    NOT NULL,
+    [FirstName] NVARCHAR (20) NOT NULL,
+    [LastName]  NVARCHAR (30) NOT NULL,
+    [LinkedECG] NCHAR (10)    NULL,
+    PRIMARY KEY CLUSTERED ([CPR] ASC),
+    FOREIGN KEY ([LinkedECG]) REFERENCES [dbo].[ECGMonitor] ([ECGMonitorID])
+);
+
