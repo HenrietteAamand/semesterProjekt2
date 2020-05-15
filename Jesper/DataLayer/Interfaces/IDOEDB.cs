8080d3aeafef6e3e96aaee5b3c71191c53e768d1
amaliekjaersgaard <amaliekjaersgaard@AMALIEKJRSG01AE>
2020-05-15 14:19:21 +0200

    Implemented DOEDB methods

--- Jesper/DataLayer/Interfaces/IDOEDB.cs
+++ Jesper/DataLayer/Interfaces/IDOEDB.cs
@@ -3,11 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DataTier.Interfaces;
+using DataTier;
+using System.Data.SqlClient;
+using DataTier.Models;
+using System.ComponentModel;
+using System.Globalization;
+using System.Threading;
 
 namespace DataTier.Interfaces
 {
     public interface IDOEDB
     {
-        void UploadData();
+        void UploadData(AnalyzedECGModel analyzedEcg);
+
+        void UploadMaeling(PatientModel patient, string workerID, string note,DateTime date);
+
     }
 }
