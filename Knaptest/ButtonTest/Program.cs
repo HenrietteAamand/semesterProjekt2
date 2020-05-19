ba4b90f29ff5b87f3fb931ccc8b44b1513de3634
LarsMoelgaard <60734425+LarsMoelgaard@users.noreply.github.com>
2020-05-19 11:06:04 +0200

    Program til test af antiprell

---
+++ Knaptest/ButtonTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using RaspberryPiCore.ADC;
+using RaspberryPiCore.TWIST;
+using RaspberryPiCore.LCD;
+
+
+namespace Raspberry_Pi_Dot_Net_Core_Console_Application3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TWIST Button = new TWIST();
+            int knaptryk = 0;
+
+            while(true)
+            {
+                if(Button.isPressed())
+                {
+                    Console.WriteLine(knaptryk + " " + DateTime.Now +":"+  DateTime.Now.Millisecond);
+                    knaptryk++;
+                }
+
+
+            }
+        }
+    }
+}
